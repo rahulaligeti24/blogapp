3305218ab86b6d4b5b413fb8313131eccb6b6794
rahulaligeti24 <aligetirahul24@gmail.com>
2025-03-28 22:39:30 +0530

    Update Header.jsx

--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -1,73 +1,85 @@
-import  { useContext } from 'react'
+import { useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import {  useClerk ,useUser} from '@clerk/clerk-react';
-import { userAuthorContextObj } from '../../contexts/UserAuthorContext';
+//import logo from "../../assets/logo4.avif";
+import { useClerk, useUser } from '@clerk/clerk-react'
+import { userAuthorContextObj } from '../../contexts/UserAuthorContext'
 
-function Header() {
+const Header = () => {
+  //const { userId } = useAuth();
+  const { signOut } = useClerk();
+  const { currentUser,setCurrentUser } = useContext(userAuthorContextObj);
+ // console.log(currentUser);
 
-          const { signOut }=useClerk()
-          const { isSignedIn,user,isLoaded }=useUser()
-       const {currentUser,setCurrentUser}=useContext(userAuthorContextObj)
-     
-     const navigate=useNavigate();
-
-
-
-        //functyiom to signout//
-       async  function handleSignOut(){
-            await signOut();
-       setCurrentUser(null)
-        navigate('/')
-
+ const navigate=useNavigate()
+  // Add these lines
+  const { isSignedIn, user, isLoaded } = useUser();
+  const handleSignOut = async () => {
+    console.log("signout called")
+    try {
+      await signOut();
+      // Clear local storage after successful sign out
+      setCurrentUser(null)
+      localStorage.clear();
+      navigate('/')
+    } catch (err) {
+      console.error("Error signing out:", err);
     }
-
-
-
+  };
 
   return (
-
-
-
-    
     <div>
-         <nav className='header d-flex justify-content-between'>
-              <div className='d-flex justify-content-center'>
-                <Link to='/'>
-                LOGO
+      <nav className="header   d-flex justify-content-between align-content-center py-3">
+        <div className="d-flex justify-content-center">
+          <Link href="/">
+            <img src={logo} alt="" width="60px" className="ms-4" />
+          </Link>
+        </div>
+        <ul className="text-white d-flex justify-content-center  list-unstyled">
+          {!isSignedIn ? (
+            <>
+              <li>
+                <Link to="signin" className=" link me-4 ">
+                  Sign In
                 </Link>
+              </li>
+              <li>
+                <Link to="signup" className="link  me-4 ">
+                  Sign Up
+                </Link>
+              </li>
+            </>
+          ) : (
+            <div
+              className="d-flex justify-content-around"
+              style={{ width: "200px" }}
+            >
+              <div className="user-button">
+                <div style={{position:"relative"}}>
+                  <img
+                    src={user.imageUrl}
+                    width="40px"
+                    className="rounded-circle"
+                    alt=""
+                    
+                  />
+                  <p className="role" style={{position:"absolute", top:"0px",right:"-20px"}}>{currentUser.role}</p>
+                </div>
+                <p className="mb-0 user-name"> {user.firstName}</p>
+                {/* <div >
+                  {currentUser.role}
+                </div> */}
               </div>
-              <ul className='d-flex list-unstyled justify-content-around header-links'>
-                
-               {
-                !isSignedIn?
-                <>
-                  <li>
-                <Link to=''>Home</Link>
-               </li>
-               <li>
-                <Link to='signin'>Signin</Link>
-               </li>
-               <li>
-                <Link to='signup'>Signup</Link>
-               </li>
-                </>:
-               
-                  
-                  <div  className='user-button container'>
-                    <div style={{position:"relative"}}>
-                       <img src={user.imageUrl} width='40px' className='rounded-circle' alt="" />
-                         <p className='role' style={{position:'absolute',top:'opx',right:'-20px'}}>{currentUser.role}</p>
-                    </div>
-                    <p className='mb-0' style={{position:'absolute'}} >{user.firstName}</p>
-                    <button className='btn btn-danger signedout-btn' onClick={handleSignOut}>SignOut</button>
-                  </div>
 
-               }
-
-              </ul>
-         </nav>
+              {/* <UserButton /> */}
+              <button onClick={handleSignOut} className="signout-btn">
+                Signout
+              </button>
+            </div>
+          )}
+        </ul>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header;
+export default Header
